caa84f5ef420a9d365fe9aa3f2f5ab29bc56f2da
user.name <nguefaherve@gmail.com>
2025-01-31 15:42:52 +0100

    Modification entiere et nouveaux fichiers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');
@@ -24,3 +25,72 @@ function moveSlide(direction) {
     const offset = -currentIndex * (images.querySelector('img').width + 10); // 10px d'écart entre les images
     images.style.transform = `translateX(${offset}px)`;
 }
+=======
+let body = document.body;
+let profile = document.querySelector(".header .flex .profile");
+
+document.querySelector("#user-btn").onclick = () => {
+  profile.classList.toggle("active");
+  searchForm.classList.remove("active");
+};
+let searchForm = document.querySelector(".header .flex .search-form");
+
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+  profile.classList.remove("active");
+};
+
+let sideBar = document.querySelector(".side-bar");
+
+document.querySelector("#menu-btn").onclick = () => {
+  sideBar.classList.toggle("active");
+  body.classList.toggle("active");
+};
+document.querySelector(".side-bar .close-side-bar").onclick = () => {
+  sideBar.classList.toggle("active");
+  body.classList.remove("active");
+};
+
+window.onscroll = () => {
+  profile.classList.remove("active");
+  searchForm.classList.remove("active");
+
+  if (window.innerWidth < 1200) {
+    sideBar.classList.toggle("active");
+    body.classList.remove("active");
+  }
+};
+
+
+let toggleBtn = document.querySelector('#toggle-btn');
+// let body = document.body;
+
+let darkMode = localStorage.getItem('dark-mode');
+
+const enableDarkMode = () => {
+  toggleBtn.classList.replace('fa-sun', 'fa-moon');
+  body.classList.add('dark');
+  localStorage.setItem('dark-mode', 'enabled');
+};
+
+const disableDarkMode = () => {
+  toggleBtn.classList.replace('fa-moon', 'fa-sun');
+  body.classList.remove('dark');
+  localStorage.setItem('dark-mode', 'disabled');
+};
+
+// Vérifier l'état du dark mode au chargement de la page
+if (darkMode === 'enabled') {
+  enableDarkMode();
+}
+
+toggleBtn.onclick = () => {
+  let darkMode = localStorage.getItem('dark-mode');
+  if (darkMode === 'enabled') {
+    disableDarkMode();
+  } else {
+    enableDarkMode();
+  }
+};
+
+>>>>>>> bc58d1b (modification du fichier et ajustement de contenu de la page home)
